629ac1597110b214965b53904aac6a91881ffe07
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2025-02-02 14:17:54 -0400

    new problem added

---
+++ Week3/missingNumber.py
@@ -0,0 +1,15 @@
+n = int(input("size: "))
+num = [n]
+m=0
+for i in range(n):
+    num.append(0)
+for i in range(0,n-1):
+    elem = int(input("give input: "))
+    num[elem] = 1
+for i in range(1,n+1):
+    print(num[i])
+    # if(num[i]==0):
+    #     m= i
+# print(f"missing number: {m}")
+        
+
